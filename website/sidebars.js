e02e4c2ffe6093386f925551d1e70160b3e782d1
tushar <tusharmandloi23@gmail.com>
2023-08-09 18:02:55 +0100

    Add side bars

--- website/sidebars.js
+++ website/sidebars.js
@@ -19,5 +19,31 @@
  */
 
 module.exports = {
-  docs: [{}],
+  docs: [
+    {
+      type: 'category',
+      label: 'Data dictionary',
+      collapsed: true,
+      items: [
+        'dictionary/dictionary-overview',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Submission',
+      collapsed: true,
+      items: [
+        'submission/submission-overview'
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Validator',
+      collapsed: true,
+      items: [
+        'validation/how-to-use',
+        'validation/error-report'
+      ],
+    }
+  ],
 };
